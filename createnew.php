7bff694746297badd829abf41a7d1569487d882f
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 01:23:24 -0700

    update account creation

--- createnew.php
+++ createnew.php
@@ -1,38 +1,46 @@
 <html>
-<head>
-<style>
-body {
-  background-color: linen;
-}
+  <head>
+    <style>
+    body {
+      background-color: #fff;
+    }
 
-h1 {
-  color: maroon;
-}
+    h1 {
+      color: #00458b;
+      font-family: "Courier New", Courier, monospace;
+      font-size: 6vmin;
+    }
 
-button {
-    margin-left: 20px;
-}
+    p {
+      color: #282828;
+      font-family: "Lucida Console", Monaco, monospace;
+      font-size: 2.2vmin;
+      padding-bottom: 2vmin;
+    }
 
-</style>
-  <title>Select User Type</title>
-</head>
+    .input-button {
+      border-radius: 1vmin;
+      font-family: "Lucida Console", Monaco, monospace;
+      font-size: 2.2vmin;
+      width: 35vmin;
+      height: 6vmin;
+    }
+    </style>
+  </head>
 
-<body>
+  <body>
+    <div style="text-align:center;">
+    <h1 style="padding-top: 20px">Account Creation</h1>
+    <p>What kind of user are you?</p>
 
-<div style="text-align:center;">
-<h1 style="padding-top: 20px">Account Creation</h1>
-<br></br>
-<b>What kind of user are you?</b>
+    <br></br>
+    <form action="newcustomer.php" method="post">
+    <input class="input-button" name="newcustomer" type="submit" value="Customer">
+    </form>
 
-<br></br>
-<form action = "newcustomer.php" method = "post">
-<input name = "newcustomer" type = "submit" value = "Customer">
-</form>
-
-<form action = "newprovider.php" method = "post">
-<input name = "newsp" type = "submit" value = "Service Provider">
-</form>
-</div>
-
-</body>
+    <form action="newprovider.php" method="post">
+    <input class="input-button" name="newsp" type="submit" value="Service Provider">
+    </form>
+    </div>
+  </body>
 </html>
