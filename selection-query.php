3c1e3b6c414bc230c34b91126263f2d0308bab0c
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 07:37:10 -0700

    update selection php

--- selection-query.php
+++ selection-query.php
@@ -1,5 +1,5 @@
 <html>
-    <head>
+  <head>
     <style>
       table {
         border-collapse: collapse;
@@ -22,20 +22,20 @@
     </style>
   </head>
 
-<?php
-function myTable($obConn, $sql) 
-{
+  <?php
+  function myTable($obConn, $sql) 
+  {
     $rsResult = mysqli_query($obConn, $sql) or die(mySqli_error($obConn));
 
     if (mysqli_num_rows($rsResult) > 0) {
-        echo "<table width=\"100%\" border=\"0\" cellspacing=\"2\"cellpadding=\"0\"><tr align=\"center\" bgcolor=\"#CCCCCC\">";
-        $i = 0;
-        while ($i < mysqli_num_fields($rsResult)) {
-            $field = mysqli_fetch_field_direct($rsResult, $i);
-            $fieldName=$field->name;
-            echo "<td><strong>$fieldName</strong></td>";
-            $i = $i + 1;
-        }
+      echo "<table><tr bgcolor=\"#00458b\">";
+      $i = 0;
+      while ($i < mysqli_num_fields($rsResult)) {
+        $field = mysqli_fetch_field_direct($rsResult, $i);
+        $fieldName=$field->name;
+        echo "<td class=\"bold-row\"><strong>$fieldName</strong></td>";
+        $i = $i + 1;
+      }
     echo "</tr>";
 
     $bolWhite = true;
@@ -43,28 +43,22 @@ function myTable($obConn, $sql)
       echo $bolWhite ? "<tr bgcolor=\"#F0F0F0\">" : "<tr bgcolor=\"#FFF\">";
         $bolWhite=!$bolWhite; 
         foreach($row as $data) {
-            echo "<td>$data</td>";
+          echo "<td>$data</td>";
         }
         echo "</tr>";
     }
     echo"</table>";
-
     }
-}
+  }
 
-include 'mpconnection.php';
-$rating = $_GET['rating'];
-$conn = OpenCon();
-$sql = "select RevieweeID, comment From review Where rating =$rating";
-
-if (isset($_GET['rating'])){
-   myTable($conn, $sql);
-}
-
-?>
-
-</body>
-
-</div>
-
+  include 'mpconnection.php';
+  $rating = $_GET['rating'];
+  $conn = OpenCon();
+  $sql = "select RevieweeID, comment From review Where rating =$rating";
+  if (isset($_GET['rating'])){
+    myTable($conn, $sql);
+  }
+  ?>
+  </body>
+  </div>
 </html>
