44c624c0128f69b6c78a12fe0e4ae8950db0ad94
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 05:05:45 -0700

    update index

--- index.php
+++ index.php
@@ -1,51 +1,66 @@
 <html>
-<head>
-<style>
-body {
-  background-color: linen;
-}
+  <head>
+    <style>
+     body {
+        background-color: #fff;
+      }
 
-h1 {
-  color: maroon;
-}
+      h1 {
+        color: #00458b;
+        font-family: "Courier New", Courier, monospace;
+        font-size: 6vmin;
+      }
 
-button {
-    margin-left: 20px;
-}
+      p {
+        color: #282828;
+        font-family: "Lucida Console", Monaco, monospace;
+        font-size: 2.2vmin;
+      }
 
-</style>
-<title>Welcome to the Service Market Place</title>
-</head>
+      .input-field {
+        font-family: "Lucida Console", Monaco, monospace;
+        border-radius: 1vmin;
+        padding-left: 1vmin;
+        width: 35vmin;
+        height: 6vmin;
+      }
 
-<div style="text-align:center;">
-<body>
+      .input-button {
+        font-family: "Lucida Console", Monaco, monospace;
+        border-radius: 1vmin;
+        font-size: 2.2vmin;
+        width: 35vmin;
+        height: 6vmin;
+      }
 
-<h1 style="padding-top: 20px">Welcome to Service Marketplace</h1>
+      .line {
+        width: 35vmin;
+      }
 
-<h3>Customer Authentication</h3>
-
-<form action = "process.php" method = "post">
-    Enter your username: <input name = "login" type = "text"> <input  type = "submit" name = "signin" value = "Sign in">
-    <br/>
-</form>
-
-<h3>Account Creation</h3>
-
-<form action = "createnew.php" method = "post">
- For new users, please sign up here: <input type = "submit" value = "Sign up">
-</form>
-
-<!--
-
-todo for aggregation and nested aggregation and division and projection
-    -->
-
-<h3>Administration</h3>
-
-<form action = "admin.php" method = "post">
-Administrator Entry:  <input  type = "submit" name = "adminsignin" value = "Continue">
-</form>
-
-</body>
-</div>
+      .input {
+        margin-bottom: 1vmin;
+      }
+    </style>
+    <title>Service Marketplace</title>
+  </head>
+  <div style="text-align:center;">
+  <body>
+  <h1 style="padding-top: 20px">Service Marketplace</h1>
+  <p>Customer Authentication</p>
+  <form action = "process.php" method = "post">
+    <div class="input"><input class="input-field" name="login" type="text" placeholder="Username"></div> 
+    <div class="input"><input class="input-button" type="submit" name="signin" value="Sign in"></div>
+  </form>
+  <hr class="line">
+  <p>Create an Account</p>
+  <form action = "createnew.php" method = "post">
+    <div class="input"><input class="input-button" type="submit" value="Sign up"></div>
+  </form>
+  <hr class="line">
+  <br>
+  <form action = "admin.php" method = "post">
+    <div class="input"><input class="input-button" type="submit" name="adminsignin" value="Admin"></div>
+  </form>
+  </body>
+  </div>
 <html>
