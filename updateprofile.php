61274799530ca2a955689197daa7d7e50b9d69dd
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 07:53:31 -0700

    update update profile

--- updateprofile.php
+++ updateprofile.php
@@ -1,70 +1,77 @@
 <html>
-<head><title> Update </title><style>
-        body {
-        background-color: linen;
-        }
+  <head>
+    <style>
+      body {
+        background-color: #fff;
+      }
 
-        h1 {
-        color: maroon;
-        }
+      h1 {
+        color: #00458b;
+        font-family: "Courier New", Courier, monospace;
+        font-size: 6vmin;
+      }
 
-        button {
-            margin-left: 20px;
-        }
-    </style></head>
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
 
-<div style="text-align:center; padding-top:30px">
-<h1>Update my Profile</h1>
-<body>
+      table, th, td {
+        border: 1px solid #808080;
+        font-family: "Lucida Console", Monaco, monospace;
+        padding: 1.2vmin;
+        text-align: left;
+      }
 
-<?php
-session_start();
+      .input {
+        margin-bottom: 1vmin;
+      }
 
-include "mpconnection.php";
-$conn = OpenCon();
+      .input-button {
+        font-family: "Lucida Console", Monaco, monospace;
+        border-radius: 1vmin;
+        font-size: 2.2vmin;
+        width: 35vmin;
+        height: 6vmin;
+      }
 
+      .bold-row {
+        color: #fff;
+        padding-top: 2.2vmin;
+        padding-bottom: 2.2vmin;
+      }
+    </style>
+  </head>
+  <div style="text-align:center; padding-top:30px">
+  <h1>Update Profile</h1>
+  <body>
+  <?php
+    session_start();
+    include "mpconnection.php";
+    $conn = OpenCon();
+    if (isset($_POST["updateprofile"])){
+      $user = $_SESSION['user'];
+    $query = mysqli_query($conn, "SELECT customer1.username, customer1.cardNumber, customer2.billingaddress, customer2.name from customer1 left join customer2 
+    on customer1.cardnumber = customer2.cardnumber where customer1.username = '$user' ");
 
-if (isset($_POST["updateprofile"])){
+    echo '<table>
+    <tr bgcolor="#00458b"> 
+    <td class="bold-row">Username</b> </td>
+    <td class="bold-row">Name</td>
+    <td class="bold-row">Billing Address</td>
+    <td class="bold-row">Card Number</td>
+    </tr>';
 
-    $user = $_SESSION['user'];
-
-$query = mysqli_query($conn, "SELECT customer1.username, customer1.cardNumber, customer2.billingaddress, customer2.name from customer1 left join customer2 
-on customer1.cardnumber = customer2.cardnumber where customer1.username = '$user' ");
-
-
-echo '<table align = "center" cellspacing = "10" cellpadding = "8">
-<tr> 
-<td align = left> <b> Username </b> </td>
-<td align = left> <b> Name </b> </td>
-<td align = left> <b> Billing Address </b> </td>
-<td align = left> <b> Card Number </b> </td>
-<tr>';
-
-$row = mysqli_fetch_row($query);
-
-echo '<tr><td align = "left">' . $row[0]
-. '</td><td align = "left">' . $row[3] . '</td><td align = "left">' .
-$row[2] . '</td><td align = "left">' . $row[1] . '</td><td align = "left">';
-
-
-echo '<tr> <form action = "updateuser.php" method = post>';
-echo '<td align = "left"></td>';
-echo '<td align = "left"></td>';
-echo '<td align = "left"><input type = submit name = changeaddress value = Change Address> </td>' ;
-echo '</tr>';
-echo "</table>";
-
-
-}
-
-
-
-
-Closecon($conn);
-
-?>
-
-</body>
-</div>
-
+    $row = mysqli_fetch_row($query);
+    echo '<tr><td>' . $row[0]
+    . '</td><td>' . $row[3] . '</td><td>' .
+    $row[2] . '</td><td>' . $row[1] . '</td></tr>';
+    }
+    Closecon($conn);
+    echo '<form action = "updateuser.php" method = post>
+    <div class="input"><input class="input-button" type=submit name=changeaddress value="Update Address"></div>
+    </form>';
+  ?>
+  </body>
+  </div>
 </html>
