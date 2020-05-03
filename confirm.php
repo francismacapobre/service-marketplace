44f5dd94f8c7c8f9decbdad4deaf1ca85c24c8d1
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 01:17:40 -0700

    update confirm

--- confirm.php
+++ confirm.php
@@ -1,56 +1,48 @@
 <html>
-<head><title>Deleting...</title>  <style>
-    body {
-    background-color: linen;
+  <head>
+    <title>Deleting Profile</title>  
+    <style>
+      body {
+        background-color: #fff;
+      }
+
+      p {
+        color: #00458b;
+        font-family: "Lucida Console", Monaco, monospace;
+        font-size: 2.2vmin;
+        padding-bottom: 2vmin;
+      }
+    </style>
+  </head>
+
+  <div style="text-align:center;">
+  <body>
+  <?php
+    session_start();
+    include "mpconnection.php";
+    $conn = OpenCon();
+    if (isset($_POST["confirm"])){
+      $user = $_SESSION['user'];
+      $query = "DELETE customer1,customer2 
+      FROM customer1 inner join customer2 on customer1.cardnumber = customer2.cardnumber
+      WHERE customer1.username = '$user'";
+
+      if (mysqli_query($conn, $query)) {
+          echo "<p>Profile deleted</p>";
+          echo "<p>Redirecting...</p>";
+          header('refresh:3; url = http://localhost/index.php');
+      } else {
+          echo "Error: " . mysqli_error($conn);
+      }
+    }
+    if (isset($_POST["deny"])){
+      echo "<p>Account deletion denied</p>";
+      echo "<p>Redirecting to login page...</p>";
+      header('refresh:3; url = http://localhost/index.php');
     }
 
-    h1 {
-    color: maroon;
-    }
-
-    button {
-        margin-left: 20px;
-    }
-
-    </style></head>
-
-<div style="text-align:center;">
-<body>
-
-<?php
-session_start();
-
-include "mpconnection.php";
-$conn = OpenCon();
-
-
-if (isset($_POST["confirm"])){
-    $user = $_SESSION['user'];
-
-    $query = "DELETE customer1,customer2 
-    FROM customer1 inner join customer2 on customer1.cardnumber = customer2.cardnumber
-    WHERE customer1.username = '$user'";
-
-    if (mysqli_query($conn, $query)) {
-        echo "<p>Profile Removed successfully</p>";
-        echo "<p>Redirecting to login page...</p>";
-        header('refresh:3; url = http://localhost/index.php');
-    } else {
-        echo "Error updating record: " . mysqli_error($conn);
-    }
-
-}
-
-if (isset($_POST["deny"])){
-    echo "<p>Will not delete profile</p>";
-    echo "<p>Redirecting to login page...</p>";
-    header('refresh:3; url = http://localhost/index.php');
-}
-
-Closecon($conn);
-
-?>
-
-</body>
-</div>
+    Closecon($conn);
+  ?>
+  </body>
+  </div>
 </html>
