cef89ba70c37b0594074c81d9a66d81cb39ce0e5
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 07:56:29 -0700

    update update to new

--- updatetonew.php
+++ updatetonew.php
@@ -1,48 +1,39 @@
 <html>
-<head><title>Updating...</title><style>
-    body {
-    background-color: linen;
-    }
+  <head>
+    <style>
+      body {
+        background-color: #fff;
+      }
 
-    h1 {
-    color: maroon;
-    }
-
-    button {
-        margin-left: 20px;
-    }
-
-    </style></head>
-<div style="text-align:center;">
-<body>
-
-<?php
-session_start();
-
-include "mpconnection.php";
-$conn = OpenCon();
-
-if(isset($_POST['newad'])){
-    
+      p {
+        color: #00458b;
+        font-family: "Lucida Console", Monaco, monospace;
+        font-size: 2.2vmin;
+        padding-bottom: 2vmin;
+      }
+    </style>
+  </head>
+  <div style="text-align:center;">
+  <body>
+  <?php
+  session_start();
+  include "mpconnection.php";
+  $conn = OpenCon();
+  if(isset($_POST['newad'])){
     $user = $_SESSION['user'];
     $query = "UPDATE customer1 inner join customer2 on customer1.cardnumber = customer2.cardnumber
     set customer2.billingaddress = '$_POST[newad]' WHERE customer1.username = '$user'";
-
     if (mysqli_query($conn, $query)) {
-        echo "<p>Record updated successfully</p>";
-        echo "<p>Redirecting to login page...</p>";
-        header('refresh:3; url = http://localhost/index.php');
+      echo "<p>Update successful</p>";
+      echo "<p>Redirecting...</p>";
+      header('refresh:3; url = http://localhost/index.php');
     } else {
-        echo "Error updating record: " . mysqli_error($conn);
+      echo "Error: " . mysqli_error($conn);
         
     }
-   
-}
-
-CloseCon($conn);
-
-?>
-
-</body>
-</div>
+  }
+  CloseCon($conn);
+  ?>
+  </body>
+  </div>
 </html>
