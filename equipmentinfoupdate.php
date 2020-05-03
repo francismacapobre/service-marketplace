8dd2609fbd506f9a481d622585756c7ba5b86c87
Francis Macapobre <f.macapobre@gmail.com>
2020-05-03 04:19:11 -0700

    update info update

--- equipmentinfoupdate.php
+++ equipmentinfoupdate.php
@@ -1,29 +1,37 @@
-<?php
+<html>
+  <head>
+    <style>
+      p {
+        color: #00458b;
+        font-family: "Lucida Console", Monaco, monospace;
+        font-size: 2.2vmin;
+        padding-bottom: 2vmin;
+      }
+    </style>
+  </head>
+  <div style="text-align:center; padding-top:30px">
+  <?php
 
-include 'mpconnection.php';
-$conn = OpenCon();
+  include 'mpconnection.php';
+  $conn = OpenCon();
 
+  $old_name = $_POST['old_name'];
+  $new_id = $_POST['new_id'];
 
-$old_name = $_POST['old_name'];
-$new_id = $_POST['new_id'];
-
-if(isset($_POST['old_name'])){
+  if(isset($_POST['old_name'])){
     if (isset($_POST['new_id'])) {
- 
-        $query = "UPDATE EquipmentUses set EquipmentUses.EquipmentID = '$new_id' WHERE EquipmentUses.EquipmentName = '$old_name'";
-    
-        if (mysqli_query($conn, $query)) {
-            echo "<p>Record updated successfully</p>";
-            echo "<p>Redirecting to equipment page...</p>";
-            header('refresh:3; url = http://localhost/equipmentinfo.html');
-        } else {
-            echo "Error updating record: " . mysqli_error($conn);
-            
-        }
+      $query = "UPDATE EquipmentUses set EquipmentUses.EquipmentID = '$new_id' WHERE EquipmentUses.EquipmentName = '$old_name'";
+      if (mysqli_query($conn, $query)) {
+        echo "<p>Update successful</p>";
+        echo "<p>Redirecting...</p>";
+        header('refresh:3; url = http://localhost/equipmentinfo.html');
+      } else {
+        echo "Error: " . mysqli_error($conn);
+      }
     }
-   
-}
-
-
-?>
+    
+  }
+  ?>
+  </div>
+</html>
 
