c6ff3428ba1adbc072e49ae7dd5e479511e02d5a
Francis Macapobre <f.macapobre@gmail.com>
2020-05-02 19:16:05 -0700

    switched mp connection to heroku server

--- mpconnection.php
+++ mpconnection.php
@@ -1,10 +1,19 @@
+
+
 <?php
 function OpenCon()
  {
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "root";
-    $db = "marketplace";
+    //Get Heroku ClearDB connection information
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server   = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db       = substr($cleardb_url["path"],1);
+
+    $dbhost = $cleardb_server;
+    $dbuser = $cleardb_username;
+    $dbpass = $cleardb_password;
+    $db = $cleardb_db;
 
     $conn = new mysqli($dbhost,$dbuser,$dbpass,$db) or die("Connect to MarketPlace fialed : %s\n". $conn ->error);
     return $conn;
